8132691bd80e2fe70b37cfc4e1286413815227d2
joegrace44 <joegrace29@gmail.com>
2025-11-18 19:29:32 +0000

    Initial commit – created project structure and classes

---
+++ 22448734_JosephGrace_Server/src/pkg22448734_josephgrace_server/ClientHandler.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg22448734_josephgrace_server;
+
+/**
+ *
+ * @author joegr
+ */
+
+public class ClientHandler implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ 22448734_JosephGrace_Server/src/pkg22448734_josephgrace_server/Event.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg22448734_josephgrace_server;
+
+/**
+ *
+ * @author joegr
+ */
+public class Event {
+}
